f6ae593619744e35d05b9f692b3f39e23510cd7b
Dawid Metelski <metel628@onet.pl>
2023-03-27 20:00:58 +0200

    get pokemons from API

--- src/App.js
+++ src/App.js
@@ -1,8 +1,30 @@
+import { useState, useEffect } from 'react';
 import './App.css';
+import axios from 'axios';
+
+const URL = 'https://pokeapi.co/api/v2/pokemon/?limit=281&offset=281'
 
 function App() {
+  
+  const [pokemon, setPokemon] = useState([])
+  
+  useEffect(() => {
+    axios.get(URL)
+    .then(response => {
+      setPokemon(response.data.results)
+      console.log(pokemon)
+    })
+  }, []);
+
   return (
-    <></>
+    <>
+    
+      {/* {
+        pokemon.map((poke, key) => {
+          return (<p key={key}>{poke.name}</p>)
+          })
+      } */}
+    </>
   );
 }
 
